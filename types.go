6e86ce6b71308d98dccba2faebab291b27d85e37
Louis Andris <mail@louisandris.com>
2015-09-21 12:03:21 +0000

    add float64 support

--- types.go
+++ types.go
@@ -24,6 +24,26 @@ func (is *Int64Slice) Scan(value interface{}) error {
 	return nil
 }
 
+type Float64Slice []float64
+
+func (fs *Float64Slice) Scan(value interface{}) error {
+	if value == nil {
+		return nil
+	}
+	val := string(value.([]byte))
+	parsed := parse(val)
+	converted := []float64{}
+	for _, v := range parsed {
+		pv, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			return nil
+		}
+		converted = append(converted, pv)
+	}
+	(*fs) = Float64Slice(converted)
+	return nil
+}
+
 type StringSlice []string
 
 func (ss *StringSlice) Scan(value interface{}) error {
